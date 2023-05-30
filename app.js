78bb8973c89428c5e3ca6b7d76ddfdd705584865
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-05-29 22:03:38 -0300

    Refactor and add CLI args support

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const fs = require("node:fs/promises");
-const getOSData = require("./lib/os-data");
+const getFile = require("./lib/file-info");
+const getCSV = require("./lib/data/csv");
+const getString = require("./lib/data/string");
+const getJSON = require("./lib/data/json");
+const InvalidExtensionError = require("./lib/errors/invalid-ext");
+const DataError = require("./lib/errors/os-info");
 
 async function getData(createFile = false, filename = "info.txt") {
   try {
-    const dataString = getOSData();
-    createFile
-      ? await fs.writeFile(filename, dataString, "utf-8")
-      : console.log(dataString);
+    if (!createFile) {
+      return console.log(getString());
+    }
+
+    const { ext } = getFile(filename);
+    const filePath = `content/${filename}`;
+
+    switch (ext) {
+      case "json":
+        return await fs.writeFile(filePath, getJSON(), "utf-8");
+      case "csv":
+        return await fs.writeFile(filePath, getCSV(), "utf-8");
+      case "txt":
+        return await fs.writeFile(filePath, getString(), "utf-8");
+      default:
+        throw new InvalidExtensionError();
+    }
   } catch (err) {
-    throw new Error("Error when getting user data\n" + err.message);
+    const message = "Error when getting user data\n" + err.message;
+    throw new DataError(message);
   }
 }
 
---
+++ content/README.md
@@ -0,0 +1,3 @@
+# Content folder 📋
+
+If you use ``--filename`` or ``--format`` arguments the generated files will appear here with the os data
